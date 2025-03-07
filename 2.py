548d927ba23a0c1e9944d178b6edc8f1309fe058
izartorius <atreuseinzbelt@gmail.com>
2025-03-07 18:16:49 +0800

    determines whether both inputs are equal or not

---
+++ 2.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+
+if num != num2:
+    print("both numbers are not equal")
+else:
+    print("both numbers are equal")
